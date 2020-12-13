eca2d9ed85237f47f5148c82fb0b7dedcdeb899d
zidan-med <71222458+zidan-med@users.noreply.github.com>
2020-12-13 22:38:31 +0100

    adding the products

--- src/main/java/org/gestionstock/GestionStockApplication.java
+++ src/main/java/org/gestionstock/GestionStockApplication.java
@@ -1,13 +1,19 @@
 package org.gestionstock;
 
+import org.gestionstock.dao.ProduitRepositry;
+import org.gestionstock.entities.Produit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class GestionStockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionStockApplication.class, args);
+		ApplicationContext ctx =SpringApplication.run(GestionStockApplication.class, args);
+		ProduitRepositry produitRepository =ctx.getBean(ProduitRepositry.class);
+		produitRepository.save(new Produit("Table", 150, "photo"));
 	}
 
 }
